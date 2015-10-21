3c09cbc7cd356dabe581139ef60dae2a95486e8f
arahnid <zhukov.vlad89@gmail.com>
2015-10-21 22:00:43 +0400

    init

---
+++ ParserVolgInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ParserVolgInfo.Core;
+
+namespace ParserVolgInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Парсер запущен, ожидайте окончания выполнения работы...");
+            ParserVI.StartParser();           
+            Console.WriteLine("Работа завершена.");
+        }
+    }
+}
